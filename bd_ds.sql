8b0d174db6c159e7be7c689a48c74f5c6b699edb
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2022-12-06 09:32:10 -0500

    script db

---
+++ bd_ds.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-01-2019 a las 22:53:44
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_ds`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre_cliente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `mail_cliente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `tel_cliente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dir_cliente` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
+  `ultima_modificacion` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Esta tabla es para registrar a los clientes del sistema.';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipos`
+--
+
+CREATE TABLE `equipos` (
+  `id_equipo` int(50) NOT NULL,
+  `id_cliente` int(50) NOT NULL,
+  `tipo_equipo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `marca` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `modelo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `num_serie` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dia_ingreso` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `mes_ingreso` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `annio_ingreso` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `observaciones` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `estatus` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ultima_modificacion` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `comentarios_tecnicos` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `revision_tecnica_de` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Esta tabla es para registrar los equipos de los clientes';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `tipo_nivel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `estatus` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `registrado_por` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Esta tabla es para registrar a los usuarios del sistema.';
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD PRIMARY KEY (`id_equipo`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  MODIFY `id_equipo` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
